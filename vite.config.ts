2cd9674b299f218bf2e7c6691841c17184483e2c
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-07-17 14:54:29 -0400

    chore(toolchain): configure vite-plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
-  fmt: {},
-  lint: {"jsPlugins":[{"name":"vite-plus","specifier":"vite-plus/oxlint-plugin"}],"rules":{"vite-plus/prefer-vite-plus-imports":"error"},"options":{"typeAware":true,"typeCheck":true}},
+	fmt: {
+		ignorePatterns: ['dist/**', '.astro/**', 'node_modules/**'],
+		semi: true,
+		singleQuote: true,
+		useTabs: true,
+	},
+	lint: {
+		ignorePatterns: ['dist/**', '.astro/**', 'node_modules/**'],
+		jsPlugins: [{ name: 'vite-plus', specifier: 'vite-plus/oxlint-plugin' }],
+		options: {
+			typeAware: true,
+			typeCheck: true,
+		},
+		rules: {
+			'vite-plus/prefer-vite-plus-imports': 'error',
+		},
+	},
+	staged: {
+		'*.{astro,js,mjs,cjs,ts,mts,cts,tsx,jsx,json,css,scss,md}': 'vp check --fix',
+		'*.astro': 'yarn astro check',
+	},
 });
